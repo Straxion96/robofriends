c840a9a7117d4c1ba28b73ac13e0857e5d45deb9
Adam Nelson <adamnelson@Adams-MacBook-Pro.local>
2024-06-05 22:23:14 -0500

    adding Hello.Css & Hello.js files

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ Hello.css'
+
+class react extends React.Component{
+    render(){
+        render(
+            <div>
+                <h1>Hello World</h1>
+                <p>Welcome to React!!</p>
+            </div>
+        );
+    }
+}
+
+export default Hello; 
