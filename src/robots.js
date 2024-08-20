70d73a0b5a8074cf71a64de1c6ce0927772aed78
Adam Nelson <adamnelson@Adams-MacBook-Pro.local>
2024-08-19 22:59:55 -0500

    added search capabitlity

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Adam Nelson',
+    username: 'Spicy',
+    email: 'BerserkerSoul@Valhalla.XD'
+  },
+  {
+    id: 2,
+    name: 'Jason Howard',
+    username: 'J-San',
+    email: 'JKH23@MusicMaster.dis'
+  },
+  {
+    id: 3,
+    name: 'Tony Meersman',
+    username: 'Big-Boi',
+    email: 'BigBoiTonTon@IDCadam.stfu'
+  },
+  {
+    id: 4,
+    name: 'Uday Patel',
+    username: 'The uuudesss',
+    email: 'GiveMeTheUuudesss@chillboi.ind'
+  },
+  {
+    id: 5,
+    name: 'Kyle Nelson',
+    username: 'Kye Kye',
+    email: 'KyeKye@LuvUTony.4ever'
+  },
+  {
+    id: 6,
+    name: 'Johnny Reding',
+    username: 'JohnnyBoi',
+    email: 'JonJon@RockNStone.DRG4life'
+  },
+  {
+    id: 7,
+    name: 'Will The-Man',
+    username: 'The legend',
+    email: 'ChillwithFreedom@bruuuhh.bombcom'
+  },
+
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
