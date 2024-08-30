217c6a613612bf811ffa8a7f41f31796d60ebc48
Adam Nelson <adamnelson@Adams-MacBook-Pro.local>
2024-08-30 18:12:29 -0500

    added transparent background behind cards

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import 'tachyons';
 
 const card = ({id, name, email}) => {
     return (
-        <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-7'>
             <img alt='img' src={`https://robohash.org/${id}?size=100x100`} />
             <div>
                 <h2>{name}</h2>
--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '1px solid black' , height: '800px'}} >
+        <div style={{overflowY: 'scroll', border: '3px solid black' , height: '800px', background: '#15151588'}} >
             {props.children}
         </div>
     );
